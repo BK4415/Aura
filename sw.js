1dc94e438c8c4c3f2badfc775c751b7624f77fb3
BK4415 <bshill443@gmail.com>
2026-06-28 23:46:51 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'aura-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/css/style.css',
+    '/js/core.js',
+    '/js/engine.js',
+    '/js/ui.js',
+    '/json/config.json'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
